9c3f441efb0a0ae33bb74e75b3a10cd953e3787d
Tom Bryden <tombryden@me.com>
2025-11-03 21:17:27 +0000

    feat: init repo

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { ignores: ["dist/**", "node_modules/**"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+]);
